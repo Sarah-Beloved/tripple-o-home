c2cc216ce18e4662d864ee0a69be21513311db7e
Francis Matthew <matthewabiodun2001@gmail.com>
2025-11-08 06:46:09 +0100

    gfgffg

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors"); 
+
+const app = express();
+connectDB();
+
+
+app.use(express.json());
+
+
+app.use(
+  cors({
+    origin: "*", 
+  })
+);
+
+
+app.use("/api/auth", require("./routes/auth"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
